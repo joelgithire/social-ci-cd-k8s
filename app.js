8c61943c6e082277fe06237576edc9b1ce5d4628
Joelmukono <jlmukono@gmail.com>
2022-02-28 00:31:40 +0300

    add ingress config

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const middleware = require('./middleware');
-const server = app.listen(port,()=>console.log("running on port :" + 3004))
+const server = app.listen(port,()=>console.log("running on port :" + port))
 const path = require('path')
 const bodyParser = require('body-parser');
 const mongoose = require("./database");
